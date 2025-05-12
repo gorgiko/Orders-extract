53ef8d7cc6193532e602fb105354c2d95db27514
gorgiko <122184841+gorgiko@users.noreply.github.com>
2025-05-12 22:00:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv() # load all the environment variables from .env
-#import os
-#api_key = os.getenv('GOOGLE_API_KEY')
+load_dotenv() # load all the environment variables from .env
+
 
 import streamlit as st
 import os
@@ -10,8 +9,6 @@ from PIL import Image
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
-print(genai)
-
 
 # function to load gemini pro vision
 
